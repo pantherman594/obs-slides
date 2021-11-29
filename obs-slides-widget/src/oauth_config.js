18ec5713f014eb00d997d50b3e121e2b049016b2
David Shen <me@pantherman594.com>
2021-11-28 23:25:39 -0500

    Remove oauth secrets

--- obs-slides-widget/src/oauth_config.js
+++
@@ -1,3 +0,0 @@
-export const client_id = '382014854214-52k0ptfbafi1dajdgnng73idge4a72jf.apps.googleusercontent.com';
-export const client_secret = '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';
-export const redirect_uri = 'https://dav.sh/oauth';
